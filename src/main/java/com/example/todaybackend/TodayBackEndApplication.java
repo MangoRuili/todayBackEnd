f9e73dd190c3e03ddf55fad7cd1d1542d9c66437
mango <2460516457@qq.com>
2021-10-11 11:49:51 +0800

    Initial commit

---
+++ src/main/java/com/example/todaybackend/TodayBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.todaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todaybackend/TodayBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
